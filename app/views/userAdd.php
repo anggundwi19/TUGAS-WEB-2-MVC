ab7e7fe2880a71422c0c6a6a00295b577469cecb
anggundwi19 <anggunn190@gmail.com>
2024-10-30 08:44:47 +0700

    Add files via upload

---
+++ app/views/userAdd.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add User</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h2>Tambah Data Baru User</h2>
+        <form action="index.php?action=create" method="POST">
+            <div class="mb-3">
+                <label for="nama" class="form-label">nama</label>
+                <input type="text" class="form-control" id="nama" name="nama" required>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <button type="submit" class="btn btn-success">Tambah Data User</button>
+            <a href="index.php" class="btn btn-secondary">Back</a>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
